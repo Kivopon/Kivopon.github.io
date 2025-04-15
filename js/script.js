816feb15bfc6506f61c969f9e81585eea211f18d
Kivopon <k.ponomarev.vl@gmail.com>
2025-04-15 19:33:06 +0300

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+let heading = document.querySelector("header");
+
+let timerId = setInterval(startHeadAnim, 100);
+
+function startHeadAnim() {
+	heading.setAttribute("data-head-anim", "start");
+	
+	clearInterval(timerId);
+}

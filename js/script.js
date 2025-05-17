f0dcc38d7f05aa01aa181ed45ff9d78fc9accf5c
Kivopon <k.ponomarev.vl@gmail.com>
2025-05-17 18:59:58 +0300

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,41 @@
 "use strict";
+// HEADER START ANIMATION
 let heading = document.querySelector("header");
 
-let timerId = setInterval(startHeadAnim, 100);
+window.addEventListener('load', startHeadAnim);
 
 function startHeadAnim() {
 	heading.setAttribute("data-head-anim", "start");
+}
+
+window.addEventListener('scroll', repeatHeadAnim); // when the page returns to the header, the animation plays again
+
+function repeatHeadAnim(event) {
+	if (window.pageYOffset >= 1080) {
+	  heading.setAttribute("data-head-anim", "reset");
+	}
 	
-	clearInterval(timerId);
+	if (window.pageYOffset <= 400 && heading.dataset.headAnim == "reset") {
+	  heading.setAttribute("data-head-anim", "start");
+	}
+}
+
+// SCROLL TO TOP
+let arrow = document.querySelector("#scroll-to-top-btn");
+
+window.addEventListener('scroll', showScrollBtn); // when the page scrolls down shows an arrow to scroll to top of the page
+
+function showScrollBtn() {
+	if (window.pageYOffset >= 1080) {
+	  arrow.setAttribute("data-arrow", "shown");
+	} else {
+	  arrow.setAttribute("data-arrow", "hidden");
+	}
+}
+
+arrow.addEventListener("click", scrollToTop); // scrolls the page to the top by clicking on the arrow
+
+function scrollToTop() {
+	window.scrollTo(0, 0);
+	arrow.setAttribute("data-arrow", "hidden");
 }
